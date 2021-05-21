b9a2a2496e3a353a02d1dc8457cf8db510878201
Christian Sandrini <mail@chrissandrini.ch>
2021-05-21 22:02:19 +0200

    Modify setup.py to make it ready for PyPi

--- setup.py
+++ setup.py
@@ -1,29 +1,32 @@
 from setuptools import setup, find_packages
+from os import path
 
-VERSION = '0.0.1' 
 DESCRIPTION = 'DefiChain exporter for Prometheus'
-LONG_DESCRIPTION = 'DefiChain master node exporter for Prometheus'
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    LONG_DESCRIPTION = f.read()
 
 # Setting up
 setup(
        # the name must match the folder name 'verysimplemodule'
-        name="defichain-exporter", 
-        version=VERSION,
+        name="defichain_exporter", 
+        version="0.0.1",
         author="Christian Sandrini",
         author_email="mail@chrissandrini.ch",
+        url="https://github.com/sandrich/defichain_exporter",
         description=DESCRIPTION,
         long_description=LONG_DESCRIPTION,
+        long_description_content_type='text/markdown',
         packages=find_packages(),
-        install_requires=[], # add any additional packages that 
+        install_requires=['jsonrpc_requests', 'prometheus-client'], # add any additional packages that 
         # needs to be installed along with your package. Eg: 'caer'
         
         keywords=['python', 'defichain', 'exporter'],
         classifiers= [
             "Development Status :: 3 - Alpha",
-            "Intended Audience :: Education",
             "Programming Language :: Python :: 3",
-            "Operating System :: MacOS :: MacOS X",
-            "Operating System :: Microsoft :: Windows",
+            "Operating System :: POSIX :: Linux"
         ],
         entry_points = {
             "console_scripts": ["defichain-exporter=defichain_exporter.__main__:main"]
